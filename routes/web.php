91b34ed098caf984f5b6cb6e24ed261cec721883
Unknown <nintenbarca-fabio@gmx.es>
2018-02-02 14:20:58 +0100

    Controladores añadidos

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('home2');
 });
 
+Route::resources([
+    'posts' => 'PostController',
+    'categorias' => 'CategoriaController'
+]);
+
 Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
