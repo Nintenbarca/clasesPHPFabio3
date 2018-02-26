5ea2265c860537c0b84e547c3ccf48e0b43fec55
Unknown <nintenbarca-fabio@gmx.es>
2018-02-26 11:52:41 +0100

    Mostrar el Botón de borrar y editar sólo al autor del Post

--- resources/views/post/detalle.blade.php
+++ resources/views/post/detalle.blade.php
@@ -18,7 +18,7 @@ use App\User;
 		<p>{{ $post->created_at->format('d/m/Y') }}</p>		
 		<p><span class="label label-info">{{ Categoria::findOrFail($post->categoria)->nombre }}</span></p>
 
-		@if(!Auth::guest())
+		@if(!Auth::guest() && $post->user_id == Auth::user()->id)
 
 		<p><a href="{{ url('/posts') }}/{{ $post->id }}/edit" class="btn btn-primary btn-sm">Editar</a></p>
 		{{ Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'delete']) }}
